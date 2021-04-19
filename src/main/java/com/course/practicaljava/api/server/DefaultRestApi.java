214470638115c14c831e283bbfa6503f5aa1a32a
vasanthitella <vasanthi.tella@aol.com>
2021-04-20 00:34:02 +0530

    Initial Commit

---
+++ src/main/java/com/course/practicaljava/api/server/DefaultRestApi.java
@@ -0,0 +1,25 @@
+package com.course.practicaljava.api.server;
+
+import java.time.LocalTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api")
+public class DefaultRestApi {
+
+	@GetMapping(value="/welcome")
+	public String welcome(){
+	
+		return "Welcome to Spring Boot";
+	}
+	
+	
+	@GetMapping(value="/time")
+	public String time(){
+		
+		return LocalTime.now().toString();
+	}
+}
